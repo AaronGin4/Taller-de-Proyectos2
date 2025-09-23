158d9084117e26acfd12f7b51645d85bbfabb2be
Aaron Giovanny <72086457@continental.edu.pe>
2025-09-22 23:44:03 -0500

    Creacion de los modulos de menu-almacen, menu-areas y registro-pasteurizado y coneccion de las rutas entre paginas, creacion de la carpeta assets y agregar imagenes

--- Codigo/Front-end/Front-end/src/app/app.routes.ts
+++ Codigo/Front-end/Front-end/src/app/app.routes.ts
@@ -1,3 +1,35 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard'; 
+import { AlmacenGuard } from './almacen.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path: '',
+      redirectTo: 'menu-areas',
+      pathMatch: 'full'
+    },
+    {
+      path: 'login',
+      loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+    },
+    {
+      path: 'menu-areas',
+      loadComponent: () => import('./menu-areas/menu-areas.component').then(m => m.MenuAreasComponent),
+        canActivate: [AuthGuard],
+    },
+    ///////////////////////////////////////
+    
+    {
+      path: 'menu-almacen',
+      loadComponent: () => import('./menu-almacen/menu-almacen.component').then(m => m.MenuAlmacenComponent),
+      canActivate: [AlmacenGuard]
+    },
+    
+    {
+    path: '**',
+    redirectTo: 'menu-areas',
+    pathMatch: 'full'
+    }
+
+ ];
