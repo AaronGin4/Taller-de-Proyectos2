ebeb39a148e495942d76afa8a4cba529f359001d
Rickf4ll <73793126@continental.edu.pe>
2025-11-10 19:19:19 -0500

    historia de usuario 17 ID: 3.1.3

---
+++ Codigo/Back-end/src/main/java/com/HospitalRegional/BancoDeLeche/entity/RegistroLecheCrudaDonadora.java
@@ -0,0 +1,77 @@
+package com.HospitalRegional.BancoDeLeche.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "Registro_Leche_Cruda_Donadora")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistroLecheCrudaDonadora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id_Leche_Cruda")
+    private Long idLecheCruda;
+
+    @Column(name = "Cantidad", nullable = false)
+    private Float cantidad;
+
+    @Column(name = "Hora", nullable = false)
+    private String hora;
+
+    @Column(name = "Fecha", nullable = false)
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "Id_Donadora", nullable = false)
+    private Donadora donadora;
+
+
+    public Long getIdLecheCruda() {
+        return idLecheCruda;
+    }
+
+    public void setIdLecheCruda(Long idLecheCruda) {
+        this.idLecheCruda = idLecheCruda;
+    }
+
+    public Float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Donadora getDonadora() {
+        return donadora;
+    }
+
+    public void setDonadora(Donadora donadora) {
+        this.donadora = donadora;
+    }
+}
